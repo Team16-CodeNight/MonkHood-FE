a235e693f1ac47232470d7cfc0e1c2d8c853d6c4
Indrakant Dana <ik.dana24@gmail.com>
2023-01-28 16:04:45 +0530

    Update Package-Lock

--- src/components/Scheduler/Calender/Calender.js
+++ src/components/Scheduler/Calender/Calender.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Calender.module.css";
 
 const Calender = () => {
-  return <div className={styles.container}>Calender</div>;
+  return <div>Calender</div>;
 };
 
 export default Calender;
