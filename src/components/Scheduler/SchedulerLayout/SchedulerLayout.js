4d243bbafcd58268b18ddceea86c7eee2a802453
Nishul Patni <nishulpatni2@gmail.com>
2023-01-28 14:06:07 +0530

    added context methods for dummy tasks and schedules

--- src/components/Scheduler/SchedulerLayout/SchedulerLayout.js
+++ src/components/Scheduler/SchedulerLayout/SchedulerLayout.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import SchedulerNavbar from "../SchedulerNavbar";
 import Calender from "../Calender/Calender";
 import Sidebar from "../Sidebar/Sidebar";
+import TasksContext from "../../../contexts/TasksContext";
 
 import styles from "./SchedulerLayout.module.css";
 
 const SchedulerLayout = () => {
+
+  // call setDummySchedules first to set dummy schedules then call getSchedules
+
+  let {getTasks, addNewTask, setDummySchedules, getSchedules, addNewSchedule} = useContext(TasksContext);
+  
   return (
     <>
       <SchedulerNavbar />
