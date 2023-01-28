35cedf9e17a02601344ee9792acd94eff21e3d9e
Indrakant Dana <ik.dana24@gmail.com>
2023-01-28 11:43:37 +0530

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">MonkHood - Your Zen Professional Planner</div>;
+  return (
+    <div className="App">
+      {/* MonkHood - Your Zen Professional Planner */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
