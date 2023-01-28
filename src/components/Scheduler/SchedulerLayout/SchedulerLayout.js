5efe71e13636a179cc766632abba5ce9cd8104f9
Indrakant Dana <ik.dana24@gmail.com>
2023-01-29 01:34:45 +0530

    Fix SchedulerLayout & Update Tasks Color Scheme

--- src/components/Scheduler/SchedulerLayout/SchedulerLayout.js
+++ src/components/Scheduler/SchedulerLayout/SchedulerLayout.js
@@ -13,8 +13,8 @@ import { async } from "@firebase/util";
 
 const SchedulerLayout = () => {
   // call setDummySchedules first to set dummy schedules then call getSchedules
-  const { getEvents } = useContext(TasksContext);
-  const { apiCalendar, handleGoogleCalenderSignUp } = useUserAuth();
+  // const { getEvents } = useContext(TasksContext);
+  const { handleGoogleCalenderSignUp, userData } = useUserAuth();
   let { component } = useParams();
   const [tab, setTab] = useState(1);
   const [tasks, setTasks] = useState([]);
@@ -26,38 +26,9 @@ const SchedulerLayout = () => {
   }, [component]);
 
   useEffect(() => {
-    console.log("called....");
-    handleGoogleCalenderSignUp("sign-in");
+    if (typeof window === undefined) handleGoogleCalenderSignUp("sign-in");
+    // console.log("called....");
   }, []);
-
-  const createEventHandler = async () => {
-    let endTime = new Date();
-    endTime.setHours(endTime.getHours() + 10);
-    let startTime = new Date();
-    startTime.setHours(startTime.getHours() + 7);
-    apiCalendar
-      .createEvent(
-        {
-          start: {
-            dateTime: startTime,
-          },
-          end: {
-            dateTime: endTime,
-          },
-          description: "Nothfasdfasdfing",
-          summary: "Ckljasdbhgf",
-        },
-        "primary"
-      )
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    console.log("called...");
-  };
 
   return (
     <>
--- src/components/Scheduler/Tasks/Tasks.js
+++ src/components/Scheduler/Tasks/Tasks.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../../../contexts/UserAuthContextProvider";
 import { PlusSmIcon, PlusIcon } from "@heroicons/react/solid";
-import { HiExternalLink, HiTrash, HiPencilAlt } from "react-icons/hi";
+import { HiExternalLink, HiTrash, HiPencilAlt, HiClock } from "react-icons/hi";
 
 import styles from "./Tasks.module.css";
 import { update } from "../../../Util/DBUtil";
@@ -25,18 +25,10 @@ const Tasks = () => {
   };
 
   return (
-    <div
-      className={`bg-white px-4 py-5 border-b border-gray-200 sm:px-6 ${styles.container}`}
-    >
+    <div className={`bg-white px-4 py-5 border-b border-gray-200 sm:px-6 ${styles.container}`}>
       {userData?.tasks?.length === 0 ? (
         <div className="text-center">
-          <svg
-            className="mx-auto h-12 w-12 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
+          <svg className="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path
               vectorEffect="non-scaling-stroke"
               strokeLinecap="round"
@@ -46,13 +38,11 @@ const Tasks = () => {
             />
           </svg>
           <h3 className="mt-2 text-sm font-medium text-gray-900">No Tasks</h3>
-          <p className="mt-1 text-sm text-gray-500">
-            Get Started with your tasks.
-          </p>
+          <p className="mt-1 text-sm text-gray-500">Get Started with your tasks.</p>
           <div className="mt-6">
             <button
               type="button"
-              className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
               onClick={() => {
                 setOpen(true);
               }}
@@ -67,22 +57,17 @@ const Tasks = () => {
         <>
           <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
             <div className="ml-4">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
-                Tasks
-              </h3>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">Tasks</h3>
             </div>
             <div className="ml-4 mb-1 mt-1 flex-shrink-0">
               <button
                 type="button"
-                className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                 onClick={() => {
                   setOpen(true);
                 }}
               >
-                <PlusSmIcon
-                  className="-ml-1 mr-2 h-5 w-5 "
-                  aria-hidden="true"
-                />
+                <PlusSmIcon className="-ml-1 mr-2 h-5 w-5 " aria-hidden="true" />
                 <span>New Task</span>
               </button>
               <AddTaskModal open={open} setOpen={setOpen} setTasks />
@@ -94,12 +79,14 @@ const Tasks = () => {
                 return (
                   <div className="relative flex items-start py-4">
                     <div className="min-w-0 flex-1 text-sm">
-                      <label
-                        htmlFor="comments"
-                        className="font-medium text-gray-700 flex flex-row items-center justify-between"
-                      >
+                      <label htmlFor="comments" className="font-medium text-gray-700 flex flex-row items-center">
                         {task?.title}
-                        <HiPencilAlt />
+                        <button
+                          type="button"
+                          className="inline-flex items-center ml-2 px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-transparent hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                          <HiPencilAlt aria-hidden="true" />
+                        </button>
                       </label>
 
                       <p id="comments-description" className="text-gray-500">
@@ -109,11 +96,15 @@ const Tasks = () => {
                         type="button"
                         className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
-                        <HiExternalLink
-                          className="-ml-0.5 mr-2 h-4 w-4"
-                          aria-hidden="true"
-                        />
-                        Sync with Calender
+                        <HiExternalLink className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                        Push to Calender
+                      </button>
+                      <button
+                        type="button"
+                        className="inline-flex items-center ml-4 px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-green-700 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                      >
+                        <HiClock className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                        Undo
                       </button>
                     </div>
                     <div className="ml-3 flex items-center h-5 flex-col">
@@ -122,11 +113,11 @@ const Tasks = () => {
                         aria-describedby="comments-description"
                         name="comments"
                         type="checkbox"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded mb-5"
+                        className="focus:ring-red-500 h-4 w-4 text-red-600 border-gray-300 rounded mb-5"
                       />
                       <button
                         type="button"
-                        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                         onClick={() => handleDeleteTask(task?.id)}
                       >
                         <HiTrash className="h-4" aria-hidden="true" />
