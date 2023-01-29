50ca5de761d00f80cd59c3dd9bfff1f8f8b3430f
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-01-29 22:53:01 +0530

    Replaced the API key

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDU-OzN51rCxxYN9XqqZh5OVoyoLwVkSZc",
-  authDomain: "monkhood-be.firebaseapp.com",
-  projectId: "monkhood-be",
-  storageBucket: "monkhood-be.appspot.com",
-  messagingSenderId: "847243250624",
-  appId: "1:847243250624:web:b9dc583616dc7e13e4d625",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
 };
 
 const app = initializeApp(firebaseConfig);
